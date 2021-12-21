e6aa7c5a3ccf4d520bab5158ee921700d106d6b0
Meydand2001 <88796084+Meydand2001@users.noreply.github.com>
2021-12-21 12:07:14 +0200

    Add files via upload

---
+++ dictionary.c
@@ -0,0 +1,60 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "dictionary.h"
+
+
+Element* allocate() {
+	Element* e = (Element*)malloc(sizeof(Element));
+	return e;
+}
+
+void destroy(Element* e) {
+	free(e);
+}
+
+void init_element(Element* e, int num, char* label) {
+	e->row_number = num;
+	strcpy(e->line, label);
+}
+
+Dictionary* allocatedict() {
+	Dictionary* d = (Dictionary*)malloc(sizeof(Dictionary));
+	if (d == NULL) {
+		return NULL;
+	}
+	return d;
+}
+
+void destroydict(Dictionary* dictionary) {
+	free(dictionary->list);
+	free(dictionary);
+}
+void init_dictionary(Dictionary* dictionary, int size) {
+	dictionary->max = size;
+	dictionary->number_of_elements = 0;
+	dictionary->list = (Element*)malloc(size * sizeof(Element));
+}
+
+void add_element(Dictionary* dictionary, Element* element) {
+	Element* list = dictionary->list;
+	init_element(&list[dictionary->number_of_elements], element->row_number, element->line);
+	dictionary->number_of_elements++;
+}
+
+int get_row(Dictionary* dictionary, char* label) {
+	Element* list = dictionary->list;
+	for (int i = 0; i < dictionary->number_of_elements; i++) {
+		if (strcmp(list[i].line, label) == 0) {
+			return list[i].row_number;
+		}
+	}
+	return 0;
+}
+
+void get_line(Dictionary* dictionary, int row_number, char* line) {
+	Element* list = dictionary->list;
+	strcpy(line, list[row_number].line);
+}
---
+++ dictionary.h
@@ -0,0 +1,26 @@
+#ifndef DICTIONARY_H
+#define DICTIONARY_H
+
+typedef struct {
+	int row_number;
+	char line[200];
+}Element;
+
+typedef struct {
+	int number_of_elements;
+	int max;
+	struct Element* list;
+}Dictionary;
+
+Element* allocate();
+void destroy(Element* e);
+void init_element(Element* e, int row, char* label);
+
+Dictionary* allocatedict();
+void destroydict(Dictionary* dictionary);
+void init_dictionary(Dictionary* dictionary, int size);
+void add_element(Dictionary* dictionary, Element* element);
+int get_row(Dictionary* dictionary, char* label);
+void get_line(Dictionary* dictionary, int row_number, char* line);
+
+#endif 
