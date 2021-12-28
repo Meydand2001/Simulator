dc2f86de6fc681ad3f5589aca8a6aa08d3162c18
gildro02 <96247470+gildro02@users.noreply.github.com>
2021-12-28 13:33:42 +0200

    Update and rename Header.h to Operations.h

--- Operations.h
+++ Operations.h
@@ -2,8 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 
-int Hex2Num(char* hex);
-int Hex2Digit(char hex);
+int hex2num(char* hex);
+int hex2digit(char hex);
 int Pow(int base, int power);
 struct Processer;
 struct Memory;
