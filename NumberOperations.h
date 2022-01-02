f16a2935e37017d086cd44be2e6927d4c26255de
Meydand2001 <88796084+Meydand2001@users.noreply.github.com>
2022-01-02 09:20:20 +0200

    Update NumberOperations.h

--- NumberOperations.h
+++ NumberOperations.h
@@ -9,5 +9,8 @@ void complete(char* bin, int l, int pos);
 void hex2bin(char num[], char* bin);
 void num2bin(int number, char* bin);
 void signednum2bin(int number, char* bin);
+void dectobin(char num[], char* bin, int v);
+void bin2hex(char bin[], char* hex, int v);
+void num2hex(int number, char* hex, int v);
 
 #endif
