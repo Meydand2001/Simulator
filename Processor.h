4030e31b4542b9d98948b0e412effdb89ddac600
Meydand2001 <88796084+Meydand2001@users.noreply.github.com>
2022-01-02 09:40:51 +0200

    Update Processor.h

--- Processor.h
+++ Processor.h
@@ -3,9 +3,14 @@
 
 #include "dictionary.h"
 
-typedef struct {
+ typedef struct {
 	int Memory[4096];
 } Memory;
+
+
+ Memory* allocated_mem();
+ void destroy_mem(Memory* memory);
+ void init_memory(Memory* memory, Dictionary* dmemin);
 
 typedef struct {
 	Dictionary* executable;
@@ -15,4 +20,11 @@ typedef struct {
 	int Flag;  //1 if processor is running, 0 if halt command was issued.
 	int isHandlingInterrupt; //1 if is in an interrupt routine, 0 if is not.
 } Processor;
+
+Processor* allocated_pro();
+void destroy_pro(Processor* SIMP);
+void init_Processor(Processor* SIMP, Dictionary* executable);
+
+void execute_code(Processor* SIMP, Memory* memory, Hard_disk* hd, Monitor* monitor);
+
 #endif
