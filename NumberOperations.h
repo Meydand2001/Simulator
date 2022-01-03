d9f5d0585023cd59c74e48ecc96edf677bf48d89
Meydand2001 <88796084+Meydand2001@users.noreply.github.com>
2022-01-03 13:59:56 +0200

    Update NumberOperations.h

--- NumberOperations.h
+++ NumberOperations.h
@@ -6,11 +6,13 @@ int hex2num(char* hex);
 int hex2digit(char hex);
 int Pow(int base, int power);
 void complete(char* bin, int l, int pos);
-void hex2bin(char num[], char* bin);
+void hex2bin(char num[], char* bin, int l);
 void num2bin(int number, char* bin);
-void signednum2bin(int number, char* bin);
-void dectobin(char num[], char* bin, int v);
-void bin2hex(char bin[], char* hex, int v);
+void pad0(char* num, int desiredLength);
+void paddednum2hex(int num, char* hex, int desiredLength);
+void signednum2bin(int number, char* bin,int l);
+void signeddec2bin(char num[], char* bin, int l);
+void bin2hex(char bin[], char* hex, int l);
 void num2hex(int number, char* hex, int v);
 
 #endif
