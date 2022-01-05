7ceab5cd9344845bc5ee8b2e996a8a0c712a551d
gildro02 <96247470+gildro02@users.noreply.github.com>
2022-01-05 14:06:12 +0200

    Update Processor.h

--- Processor.h
+++ Processor.h
@@ -14,16 +14,16 @@
 
 
 typedef struct {
-	int pixels[256 * 256]; //
-
-}Monitor;
+	int pixels[256 * 256]; //256*256(=65536) array of pixel
+} Monitor;
 
 
 typedef struct {
 	int disk[16384]; // 128 sectors * 128 words per sector(512 bytes)
-	int sector_size;
-	int cyclesSinceStart;
-}Harddisk;
+	int sector_size; //sector_size=128.
+	int cyclesSinceStart; //counts cycles since starting read/write operation,
+						  //resets to 1 when starting a new read/write operation.
+} HardDisk;
 
 
 typedef struct {
@@ -31,43 +31,45 @@ typedef struct {
 } Memory;
 
 
-Memory* allocated_mem();
-void destroy_mem(Memory* memory);
-void init_memory(Memory* memory, Dictionary* dmemin);
-
 typedef struct {
 	Dictionary* executable;
 	Dictionary* irq2in;
-	int Registers[16];
-	int IO_Registers[23];
+	int Registers[16];    //array of all regular (non-IO) Registers
+	int IO_Registers[23]; //array of all IO Registers
 	int PC;
-	int Flag;  //1 if processor is running, 0 if halt command was issued.
+	int Flag;  //1 if processor is running, 0 if halt instruction was issued.
 	int isHandlingInterrupt; //1 if is in an interrupt routine, 0 if is not.
-	int hasJumped; // 1 if there was a jump/branch 0 otherwise.
+	int hasJumped; //1 if there was a jump/branch,0 otherwise.
 } Processor;
 
+/********Timer Functions*********/
 void timer_handler(Processor* SIMP);
 
 
+/********Monitor Functions*********/
 Monitor* allocatemonitor();
 void init_monitor(Monitor* m);
 void write_pixel(Monitor* m, int address, int value);
 void monitor_handler(Monitor* monitor, Processor* SIMP);
-void get_pixel_content(Monitor* m, int content[]);
 
 
-Harddisk* allocatedisk();
-void init_hard_disk(Harddisk* hd, Dictionary* diskin);
-//void get_content(Harddisk* hd, int content[]);
-void read_sector(Memory* memory, Harddisk* hd, int sector, int buffer);
-void write_sector(Memory* memory, Harddisk* hd, int sector, int buffer);
-void hard_disk_handler(Memory* memory, Harddisk* hd, Processor* SIMP);
+/********HardDisk Functions*********/
+HardDisk* allocatedisk();
+void init_hard_disk(HardDisk* hd, Dictionary* diskin);
+void read_sector(Memory* memory, HardDisk* hd, int sector, int buffer);
+void write_sector(Memory* memory, HardDisk* hd, int sector, int buffer);
+void hard_disk_handler(Memory* memory, HardDisk* hd, Processor* SIMP);
 
+/********Memory Functions*********/
+Memory* allocate_mem();
+void destroy_mem(Memory* memory);
+void init_memory(Memory* memory, Dictionary* dmemin);
 
-Processor* allocated_pro();
+/********Processor Functions*********/
+Processor* allocate_pro();
 void destroy_pro(Processor* SIMP);
 void init_Processor(Processor* SIMP, Dictionary* executable, Dictionary* irq2in);
-void execute_code(Processor* SIMP, Memory* memory, Harddisk* hd, Monitor* monitor, Dictionary* trace,
+void execute_code(Processor* SIMP, Memory* memory, HardDisk* hd, Monitor* monitor, Dictionary* trace,
 	Dictionary* hwregtrace, Dictionary* leds, Dictionary* display);
 
 #endif
